a443527a504d3db29ea241d24ecde24deaecdbbc
ArunAudify <arunvenkatesan30@gmail.com>
2022-11-11 23:47:13 +0530

    Created UI (xml)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CryptoJuno
  Assignment 
+
+Added 3rd party libraries:- 
+1. Retrofit
+2. ViewModel
+3. Glide
+4. Coroutines
+5. Gson 
+
+
--- app/src/main/java/com/arun/cryptojuno/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/arun/cryptojuno/ui/view/activity/MainActivity.kt
@@ -3,10 +3,17 @@ package com.arun.cryptojuno.ui.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arun.cryptojuno.R
+import com.arun.cryptojuno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
     }
 }
